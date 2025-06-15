002a4b942a5fe2a3f0c9b200911443ce917d7ff7
EffectiveDesign2025 <contato.effectivedesign@gmail.com>
2025-06-15 10:41:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -39,5 +39,5 @@ setInterval(setRandomGradient, 10000);
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: 'https://raw.githubusercontent.com/EffectiveDesign2025/effectivedesign.github.io/main/animacao/analise%20de%20projeto.json'
+        path: 'animacao/analise de projeto.json'
     });
